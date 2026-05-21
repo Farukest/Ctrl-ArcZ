10fba15aed129150e60593c9554f60e11c4b68cc
Farukest <abdullahfarukozden@gmail.com>
2026-05-21 14:54:03 +0300

    feat(demo-kit): export the test provider

--- packages/demo-kit/src/index.ts
+++ packages/demo-kit/src/index.ts
@@ -9,3 +9,4 @@ export {
   type Session,
 } from './session.js';
 export { useSession, type SessionState } from './useSession.js';
+export { makeTestProvider, installTestProvider } from './testProvider.js';
