85548862f2a0b7fc27065d024d071c4be8846c15
Farukest <abdullahfarukozden@gmail.com>
2026-05-27 15:54:43 +0300

    feat(demo-kit): export the layout pieces

--- packages/demo-kit/src/ui/index.ts
+++ packages/demo-kit/src/ui/index.ts
@@ -3,3 +3,7 @@ import './components.css';
 
 export * from './components.js';
 export * from './icons.js';
+export { RiskCard } from './RiskCard.js';
+export { ConnectBar } from './ConnectBar.js';
+export { TopBar } from './TopBar.js';
+export { LogoWordmark } from './Logo.js';
